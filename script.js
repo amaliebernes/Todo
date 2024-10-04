c7b6426fed3c1a2337668310171888b46645a66c
amaliebernes <bernesamalie@gmail.com>
2024-10-04 13:54:02 +0200

    Todo list element CreateTodoCard

--- script.js
+++ script.js
@@ -2,31 +2,88 @@
 
 
 
- let todoListElement = document.querySelector("todo-list");
+ let todoListElement = document.querySelector("#todo-list");
+
+ let todos = []
+
+ //dette er det som kommer til å skje nåe brukeren trykker på knappen
 
  function handleSubmit (event) {
 
 
 event.preventDefault(); //forhindere nettsiden å laste inn på nytt (refresh)
 
-    console.log("Form Submitted");
 
-    console.log("Creating Todo Object...");
-createTodoObject(userInput);
- }
+console.log("Creating Todo Object...");
+ let newTodo = createTodoObject(userInput);
+
+console.log("Append new todo list");
+
+todos.push(newTodo);
+
+console.log("all todos to html...");
+todoListElement.innerHTML = "";
+
+todos.forEach((todo, index) => {
+
+//Først lag det nye html gjøremål 
+let newTodoCard = createTodoCard(todo);
+
+// Legg det nye html element til listen
+
+todoListElement.append(newTodoCard);
+
+
+});
+
+
+}
  
 userInput.addEventListener("submit", handleSubmit);
 
+// Denne leser av et form element
+// Og lager et js objekt for gjøremålene
+
 function createTodoObject(form) {
 
-    console.log(form);
 
    let todo = form.querySelector("#todo");
-   console.log (todo);
 
    let todoValue = todo.value;
-   console.dir(todoValue);
+
+
+   let todoObject = {
+
+title: todoValue,
+//createdAt: "2024"
+   };
+
+   console.log(todoObject);
   
+return todoObject;
+}
+
+function createTodoCard(todoObject) {
+
+    //lag alle elementene
+
+
+let todoCard = document.createElement("li");
+
+let titleElement = document.createElement("h2");
+
+//sett de sammen til et 
+
+todoCard.append(titleElement);
+
+//konfigruer elementene med korrekt verdier
+
+titleElement.textContent = todoObject.title;
+
+return todoCard; 
+
 
 }
+
+
  
