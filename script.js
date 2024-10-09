7450ceb4330814db8bc503fcc2e08c367cfcfcad
amaliebernes <bernesamalie@gmail.com>
2024-10-09 13:21:28 +0200

    Let tudos If Else

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
 
 
-let userInput = document.querySelector ("#user-input");
+let userInput = document.querySelector("#user-input");
 
 
 
- let todoListElement = document.querySelector("#todo-list");
+let todoListElement = document.querySelector("#todo-list");
 
 
 let storedTodos = localStorage.getItem("todos");
 let convertedTodos = JSON.parse(storedTodos);
 
- let todos = convertedTodos;
+let todos;
 
- //dette er det som kommer til å skje nåe brukeren trykker på knappen
+if (storedTodos === null) {
+   todos = [];
+// Viss det er første besøk på siden, lag en ny liste
 
+}
 
- renderTodos ();
+else {
 
- 
- function handleSubmit (event) {
-event.preventDefault(); //forhindere nettsiden å laste inn på nytt (refresh)
+// Hvis det var noe lagret i localStorage, bruk det
+   todos = convertedTodos
+}
 
-
-
-console.log("Creating Todo Object...");
- let newTodo = createTodoObject(userInput);
-
-console.log("Append new todo list");
-
-todos.push(newTodo);
-
-console.log("Update the stord list");
-let jsonTodos = JSON.stringify(todos);
-localStorage.setItem("todos", jsonTodos);
 
 renderTodos();
 
+
+//dette er det som kommer til å skje nåe brukeren trykker på knappen
+
+function handleSubmit(event) {
+   event.preventDefault(); //forhindere nettsiden å laste inn på nytt (refresh)
+
+
+
+   console.log("Creating Todo Object...");
+   let newTodo = createTodoObject(userInput);
+
+   console.log("Append new todo list");
+
+   todos.push(newTodo);
+
+   console.log("Update the stord list");
+   let jsonTodos = JSON.stringify(todos);
+   localStorage.setItem("todos", jsonTodos);
+
+   renderTodos();
+
 };
- 
+
 userInput.addEventListener("submit", handleSubmit);
 
 // Denne leser av et form element
@@ -53,56 +65,56 @@ function createTodoObject(form) {
 
    let todoObject = {
 
-title: todoValue,
-//createdAt: "2024"
+      title: todoValue,
+      //createdAt: "2024"
    };
 
    console.log(todoObject);
-  
-return todoObject;
+
+   return todoObject;
 }
 
 function createTodoCard(todoObject) {
 
-    //lag alle elementene
+   //lag alle elementene
 
 
-let todoCard = document.createElement("li");
+   let todoCard = document.createElement("li");
 
-let titleElement = document.createElement("h2");
+   let titleElement = document.createElement("h2");
 
-//sett de sammen til et 
+   //sett de sammen til et 
 
-todoCard.append(titleElement);
+   todoCard.append(titleElement);
 
-//konfigruer elementene med korrekt verdier
+   //konfigruer elementene med korrekt verdier
 
-titleElement.textContent = todoObject.title;
+   titleElement.textContent = todoObject.title;
 
-return todoCard; 
+   return todoCard;
 
 }
 
 
 //Denne har jobben å oppdatere HTML
-function renderTodos () { 
-   
-console.log("all todos to html...");
-todoListElement.innerHTML = "";
+function renderTodos() {
 
-todos.forEach((todo, index) => {
+   console.log("all todos to html...");
+   todoListElement.innerHTML = "";
 
-//Først lag det nye html gjøremål 
-let newTodoCard = createTodoCard(todo);
+   todos.forEach((todo, index) => {
 
-// Legg det nye html element til listen
+      //Først lag det nye html gjøremål 
+      let newTodoCard = createTodoCard(todo);
 
-todoListElement.append(newTodoCard);
+      // Legg det nye html element til listen
+
+      todoListElement.append(newTodoCard);
 
 
 
-}
-)
-;
+   }
+   )
+      ;
 
 }
