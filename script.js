23497a6aefebe74b6f8f13a79a8f06aa7dd7c447
amaliebernes <bernesamalie@gmail.com>
2024-10-16 12:48:02 +0200

    Styling

--- script.js
+++ script.js
@@ -24,15 +24,13 @@ renderTodos();
 function handleSubmit(event) {
   event.preventDefault(); //forhindere nettsiden å laste inn på nytt (refresh)
 
-  console.log("Creating Todo Object...");
   let newTodo = createTodoObject(userInput);
 
 
-  console.log("Append new todo list");
 
   todos.push(newTodo);
 
-  console.log("Update the stord list");
+  
   let jsonTodos = JSON.stringify(todos);
   localStorage.setItem("todos", jsonTodos);
 
@@ -54,7 +52,6 @@ function createTodoObject(form) {
     //createdAt: "2024"
   };
 
-  console.log(todoObject);
 
   return todoObject;
 }
@@ -76,13 +73,12 @@ function createTodoCard(todoObject) {
   //konfigruer elementene med korrekt verdier
   todoCard.className ="bg-green-200 w-full flex justify-between";
   titleElement.className ="underline";
-  deleteButton.className ="bg-red-400 px-4 py-2 rounded hover:bg-red-300";
+  deleteButton.className ="bg-blue-500 px-4 py-2 rounded hover:bg-blue-300";
 
   titleElement.textContent = todoObject.title;
   deleteButton.textContent = "Delete";
   deleteButton.addEventListener("click", () => {
-console.log("deleting todo...");
-console.log(todoObject);   
+ 
 
    //Fjern todo object fra todos listen
 
@@ -103,7 +99,7 @@ return true
 
 })
 
-console.log(filteredTodos);
+
 
 todos = filteredTodos;
 let jsonTodos = JSON.stringify(todos);
@@ -118,7 +114,7 @@ renderTodos();
 
 //Denne har jobben å oppdatere HTML
 function renderTodos() {
-  console.log("all todos to html...");
+  
   todoListElement.innerHTML = "";
 
   todos.forEach((todo, index) => {
